c0b3257a1bd1795872b9b08f8011ce886f07d4f0
MohnishR <mv4533@srmist.edu.in>
2022-12-30 10:22:27 +0530

    Checking basic movements

--- ArduinoCar_V1/ArduinoCar_V1.ino
+++ ArduinoCar_V1/ArduinoCar_V1.ino
@@ -1,85 +1,49 @@
-int motor1pin1 = 2;
-int motor1pin2 = 3;
+int Out1 = 2;
+int Out2 = 3;
 
-int motor2pin1 = 4;
-int motor2pin2 = 5;
+int Out3 = 4;
+int Out4 = 5;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motor1pin1, OUTPUT);
-  pinMode(motor1pin2, OUTPUT);
+  pinMode(Out1, OUTPUT);
+  pinMode(Out2, OUTPUT);
 
-  pinMode(motor2pin1, OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
+  pinMode(Out3, OUTPUT);
+  pinMode(Out4, OUTPUT);
 
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, LOW);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, LOW);
+  digitalWrite(Out1, LOW);
+  digitalWrite(Out2, LOW);
+  digitalWrite(Out3, LOW);
+  digitalWrite(Out4, LOW);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  // motor 1 moves in one direction
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-  delay(2000);
-  // motor 1 moves in the opposite direction
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-  delay(2000);
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, LOW);
-
   
-  // motor 2 moves in one direction
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-  delay(2000);
-  // motor 2 moves in the opposite direction
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  delay(2000);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, LOW);
+  //Move forward
+  digitalWrite(Out1, HIGH);
+  digitalWrite(Out2, LOW);
+  digitalWrite(Out3, LOW);
+  digitalWrite(Out4, HIGH);
 
+  //Move backwards
+  digitalWrite(Out1, LOW);
+  digitalWrite(Out2, HIGH);
+  digitalWrite(Out3, HIGH);
+  digitalWrite(Out4, LOW);
 
-  // both motors moves in the same direction
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
+  //Turn right
+  digitalWrite(Out1, HIGH);
+  digitalWrite(Out2, LOW);
+  digitalWrite(Out3, HIGH);
+  digitalWrite(Out4, LOW);
 
-  delay(2000);
-
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-
-  delay(2000);
-
-  // both motors moves in opposite directions
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-
-  delay(2000);
-
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-
-  delay(2000);
-
-  //stop all motors
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, LOW);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, LOW);
+  //Move forward
+  digitalWrite(Out1, LOW);
+  digitalWrite(Out2, HIGH);
+  digitalWrite(Out3, LOW);
+  digitalWrite(Out4, HIGH);
 }
 
