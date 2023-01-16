3cca4a7e6445de41d26daf2e70cddcb26848978b
MohnishR <mv4533@srmist.edu.in>
2023-01-16 14:59:47 +0530

    Static Input Movement Check using the concept of Differential Drive Working: 	Manipulating speed of each sides motors so that object turns in a curved manner.

--- ArduinoCar_V1/ArduinoCar_V1.ino
+++ ArduinoCar_V1/ArduinoCar_V1.ino
@@ -4,6 +4,9 @@ int Out2 = 3;
 int Out3 = 4;
 int Out4 = 5;
 
+int ENA = 10;
+int ENB = 9;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(Out1, OUTPUT);
@@ -11,6 +14,9 @@ void setup() {
 
   pinMode(Out3, OUTPUT);
   pinMode(Out4, OUTPUT);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 
   digitalWrite(Out1, LOW);
   digitalWrite(Out2, LOW);
@@ -23,6 +29,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   //Move forward
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
   digitalWrite(Out1, HIGH);
   digitalWrite(Out2, LOW);
   digitalWrite(Out3, LOW);
@@ -31,6 +39,8 @@ void loop() {
   delay(2000);
 
   //Move backwards
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
   digitalWrite(Out1, LOW);
   digitalWrite(Out2, HIGH);
   digitalWrite(Out3, HIGH);
@@ -39,16 +49,20 @@ void loop() {
   delay(2000);
 
   //Turn right
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 100);
   digitalWrite(Out1, HIGH);
   digitalWrite(Out2, LOW);
-  digitalWrite(Out3, HIGH);
-  digitalWrite(Out4, LOW);
+  digitalWrite(Out3, LOW);
+  digitalWrite(Out4, HIGH);
 
   delay(2000);
 
   //Move left
-  digitalWrite(Out1, LOW);
-  digitalWrite(Out2, HIGH);
+  analogWrite(ENA, 100);
+  analogWrite(ENB, 255);
+  digitalWrite(Out1, HIGH);
+  digitalWrite(Out2, LOW);
   digitalWrite(Out3, LOW);
   digitalWrite(Out4, HIGH);
 
