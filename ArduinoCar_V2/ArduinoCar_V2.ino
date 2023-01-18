8c358d68edc7453f2c48341ef395e0f8967613fb
MohnishR <mv4533@srmist.edu.in>
2023-01-18 18:03:14 +0530

    additional directional changes added to code

--- ArduinoCar_V2/ArduinoCar_V2.ino
+++ ArduinoCar_V2/ArduinoCar_V2.ino
@@ -18,9 +18,6 @@ void setup() {
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
 
-  digitalWrite(ENA, HIGH);
-  digitalWrite(ENB, HIGH);
-
   Serial.begin(9600);
 }
 
@@ -29,31 +26,71 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()){
     String Move = Serial.readString();
-    if (Move == "F"){                         // code to make car move forward
-      digitalWrite(Out1, HIGH);
+    if (Move == "F"){             // code to make car move forward
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 255);    
+      digitalWrite(Out1, HIGH);      
       digitalWrite(Out2, LOW);
       digitalWrite(Out3, LOW);
       digitalWrite(Out4, HIGH);
     }
-    else if (Move == "B"){                    // code to make car move backward
+    else if (Move == "B"){        // code to make car move backward
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 255);
       digitalWrite(Out1, LOW);
       digitalWrite(Out2, HIGH);
       digitalWrite(Out3, HIGH);
       digitalWrite(Out4, LOW);
     }
-    else if (Move == "L"){                    // code to make car turn left
+    else if (Move == "L"){        // code to make car turn left on spot
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 255);
       digitalWrite(Out1, LOW);
       digitalWrite(Out2, HIGH);
       digitalWrite(Out3, LOW);
       digitalWrite(Out4, HIGH);
     }
-    else if (Move == "R"){                    // code to make car turn right
+    else if (Move == "R"){        // code to make car turn right on spot
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 255);
       digitalWrite(Out1, HIGH);
       digitalWrite(Out2, LOW);
       digitalWrite(Out3, HIGH);
       digitalWrite(Out4, LOW);
     }
-    else if (Move == "S"){                    // code to make car stop
+    else if (Move == "FR"){       // code to make car turn right while moving forwards
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 150);
+      digitalWrite(Out1, HIGH);      
+      digitalWrite(Out2, LOW);
+      digitalWrite(Out3, LOW);
+      digitalWrite(Out4, HIGH);
+    }
+    else if (Move == "FL"){       // code to make car turn left while moving forwards
+      analogWrite(ENA, 150);
+      analogWrite(ENB, 255);
+      digitalWrite(Out1, HIGH);      
+      digitalWrite(Out2, LOW);
+      digitalWrite(Out3, LOW);
+      digitalWrite(Out4, HIGH);
+    }
+    else if (Move == "BR"){       // code to make car turn right while moving backwards
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 150);
+      digitalWrite(Out1, LOW);
+      digitalWrite(Out2, HIGH);
+      digitalWrite(Out3, HIGH);
+      digitalWrite(Out4, LOW);
+    }    
+    else if (Move == "BL"){       // code to make car turn left while moving backwards
+      analogWrite(ENA, 150);
+      analogWrite(ENB, 255);
+      digitalWrite(Out1, LOW);
+      digitalWrite(Out2, HIGH);
+      digitalWrite(Out3, HIGH);
+      digitalWrite(Out4, LOW);
+    }        
+    else if (Move == "S"){        // code to make car stop
       digitalWrite(Out1, LOW);
       digitalWrite(Out2, LOW);
       digitalWrite(Out3, LOW);
